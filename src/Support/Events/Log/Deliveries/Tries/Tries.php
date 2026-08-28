82c7a549a5177873e5351340debc3a2ccff92c10
Justin Seliga <justinsel@zillowgroup.com>
2026-08-28 12:27:16 -0400

    mark `#[Tries]` as deprecated

--- src/Support/Events/Log/Deliveries/Tries/Tries.php
+++ src/Support/Events/Log/Deliveries/Tries/Tries.php
@@ -5,12 +5,14 @@ declare(strict_types=1);
 namespace Support\Events\Log\Deliveries\Tries;
 
 use Attribute;
+use Deprecated;
 
 #[Attribute(Attribute::TARGET_CLASS)]
 final class Tries
 {
     public readonly int $count;
 
+    #[Deprecated('Class will be removed in favor of `#[Tries]` provided by Laravel 13.')]
     public function __construct(int $count)
     {
         $this->count = $count;
